5b6eb65b9ec2a034c4674b7e80559338c1c74c11
Pratheesh Eswaran <pratheesh@Pratheeshs-MacBook-Pro.local>
2021-02-25 16:49:35 +0530

    added more spinners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './fakeleghorse.jpg';
+import logo from './fakeleghorse.png';
 import './App.css';
 
 function App() {
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <a href='https://rocknspin.surge.sh' target='_blank'>more spinn'</a>
       </header>
+      
     </div>
   );
 }
